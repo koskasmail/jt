b97e3fff23b6f4427b0e2f2fcb5fb55837b68bc6
kessler yaron <koskasmail@gmail.com>
2022-08-24 18:31:22 +0300

    CalculateAverageOfAllTheArrayElement

--- unsorted/arrays/intArray/CalculateAverageOfAllTheArrayElement.java
+++ unsorted/arrays/intArray/CalculateAverageOfAllTheArrayElement.java
@@ -1,9 +1,8 @@
-
 public class CalculateAverageOfAllTheArrayElement {
 
 	public CalculateAverageOfAllTheArrayElement() {
 		int[] intArray = new int[] { 6, 7, 10, 7, 9 };
-		int sum = 0;
+		float sum = 0;
 		
 		System.out.println("Total Grade: ");
 		for (int index = 0; index < intArray.length; index++) {
@@ -11,8 +10,8 @@ public class CalculateAverageOfAllTheArrayElement {
 			System.out.print(intArray[index] + ", ");
 		}
 		
-		float average = sum / intArray.length;
-		System.out.println("\n");
+		float average = (sum / intArray.length);
+		System.out.println("\n" + sum + " / " + intArray.length + " === " + average);
 		System.out.println("average is: " + average);
 	}
 }
