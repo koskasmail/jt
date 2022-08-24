b25f4d028501f1936d6cfb4333991f7b4141d6e4
kessler yaron <koskasmail@gmail.com>
2022-08-24 17:41:55 +0300

    ArrayOneDimension_int

--- unsorted/arrays/intArray/ArrayOneDimension_int.java
+++ unsorted/arrays/intArray/ArrayOneDimension_int.java
@@ -2,11 +2,12 @@
 public class ArrayOneDimension_int {
 
 	public ArrayOneDimension_int() {
-		int[] intArray = new int[] {1,3,5,7,9};
-		
+		int[] intArray = new int[] { 1, 3, 5, 7, 9 };
+
 		System.out.println("print intArray: ");
-		
-		for (int index=0; index< intArray.length; index++) {
+
+		for (int index = 0; index < intArray.length; index++) {
 			System.out.print(intArray[index] + ", ");
 		}
 	}
+}
