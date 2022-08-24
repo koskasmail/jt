f0ab3603a91663c33a04074348f0e97642e25a7d
kessler yaron <koskasmail@gmail.com>
2022-08-24 17:54:45 +0300

    ConvertArrayToString

---
+++ unsorted/arrays/intArray/ConvertArrayToString.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class ConvertArrayToString {
+
+	public ConvertArrayToString() {
+
+		int[] intArray = new int[] { 1, 5, 3, 9, 7 };
+		System.out.println("Convert Array To String");
+		System.out.println(Arrays.toString(intArray));
+	}
+}
