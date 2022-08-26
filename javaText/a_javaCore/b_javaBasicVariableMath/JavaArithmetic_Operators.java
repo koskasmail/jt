bb3bf5aa33ace89c6fdad825115f7e17973df5f9
yaron <yaron.kessler41@gmail.com>
2022-08-26 09:31:52 +0300

    jt

--- javaText/a_javaCore/b_javaBasicVariableMath/JavaArithmetic_Operators.java
+++ javaText/a_javaCore/b_javaBasicVariableMath/JavaArithmetic_Operators.java
@@ -5,7 +5,6 @@ public class JavaArithmeticOperators {
 	public JavaArithmeticOperators() {
 
 		System.out.println("\nJava Arithmetic Operators\n");
-
 		System.out.println("6 + 2 = " + (6 + 2));
 		System.out.println("6 - 2 = " + (6 - 2));
 		System.out.println("6 * 2 = " + (6 * 2));
